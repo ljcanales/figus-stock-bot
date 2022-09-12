2697488bcc71e82796548e7d971e2e2bc1a11cf0
ljcanales <ljcanales1404@gmail.com>
2022-09-11 23:00:29 -0300

    Add count days without stock

--- check_stock/__init__.py
+++ check_stock/__init__.py
@@ -14,7 +14,7 @@ DOCUMENT_ID = "static-id-434d8917-bcf7-44a6-aa30-7cd84159b39a"
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    utc_timestamp = datetime.datetime.utcnow().isoformat()
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
     try:
@@ -33,8 +33,14 @@ def main(mytimer: func.TimerRequest) -> None:
                 new_fields = {"last_update": utc_timestamp}
                 if doc.get("status").upper() != value.upper():
                     new_fields["status"] = value
+                    new_fields["last_status_change"] = utc_timestamp
                     new_fields["last_tweet_id"] = tweet_status(value)
-                collection.update_one({"_id": DOCUMENT_ID}, {'$set' : new_fields})
+                elif doc.get("status").upper() != "SIN STOCK":
+                    last_update = datetime.datetime.fromisoformat(doc.get("last_update"))
+                    last_status_change = datetime.datetime.fromisoformat(doc.get("last_status_change"))
+                    if (last_update - last_status_change).days < (datetime.datetime.utcnow() - last_status_change).days:
+                        post_tweet(f"🔴 STOCK AGOTADO 🔴\nEl pack x25 sobres de figuritas del mundial se encuentra SIN STOCK hace {(datetime.datetime.utcnow() - last_status_change).days} día/s.")
+                collection.update_one({"_id": DOCUMENT_ID}, {'$set': new_fields})
                 logging.info({'level': 'INFO', 'message': f'Status UPDATED with status [{value}].', 'name': 'check_stock_function'})
             else:
                 last_tweet_id = tweet_status(value)
@@ -42,6 +48,7 @@ def main(mytimer: func.TimerRequest) -> None:
                     "_id": DOCUMENT_ID,
                     "status": value,
                     "last_update": utc_timestamp,
+                    "last_status_change": utc_timestamp,
                     "last_tweet_id": last_tweet_id
                 })
                 logging.info({'level': 'INFO', 'message': f'Status CREATED with status [{value}].', 'name': 'check_stock_function'})
@@ -55,19 +62,23 @@ def main(mytimer: func.TimerRequest) -> None:
 def tweet_status(value: str) -> str:
     try:
         if value.upper() == "SIN STOCK":
-            tweet_text = "🔴 STOCK AGOTADO 🔴"
+            tweet_text = "🔴 STOCK AGOTADO 🔴\nEl pack x25 sobres de figuritas del mundial se encuentra agotado."
         else:
-            tweet_text = f"🟢 HAY STOCK 🟢\nConseguí en {URL}"
+            tweet_text = f"🟢 HAY STOCK 🟢\nEl pack x25 sobres de figuritas del mundial se encuentra con stock disponible.\n Conseguilo en {URL}"
         tweet_text += "\n\nSeguime y activá notificaciones para estar informado/a."
 
-        twitter_response = requests.post(
-            'https://api.twitter.com/2/tweets',
-            json={"text": tweet_text},
-            auth=OAuth1(os.environ['api_key'], os.environ['api_secret'], os.environ['access_key'], os.environ['access_secret'])
-        )
-
-        logging.info({'level': 'INFO', 'message': 'Status tweeted.', 'name': 'check_stock_function'})
-        return twitter_response.json()['data']['id']
+        return post_tweet(tweet_text)
     except Exception as e:
         logging.error({'level': 'ERROR', 'message': 'Error tweeting.', 'exception': str(e), 'name': 'check_stock_function'})
         return None
+
+
+def post_tweet(tweet_text: str) -> str:
+    twitter_response = requests.post(
+        'https://api.twitter.com/2/tweets',
+        json={"text": tweet_text},
+        auth=OAuth1(os.environ['api_key'], os.environ['api_secret'], os.environ['access_key'],
+                    os.environ['access_secret'])
+    )
+    logging.info({'level': 'INFO', 'message': 'Status tweeted.', 'name': 'check_stock_function'})
+    return twitter_response.json()['data']['id']
